af4f3afb05505661405bf953db4fe469a52441a3
Brent Mercado <brent.mercado.566@my.csun.edu>
2018-05-22 20:31:39 -0700

    Setup views
    Set up landing page and drumsets page with hbs view engine. Both render a basic page with header and footer partials. Bootstrap 4 css file was added to the header as a link tag. Bootstrap 4 js files were added to the footer as a script.

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var express     = require("express");
 var app         = express();
 var bodyParser  = require("body-parser");
+var hbs         = require("hbs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', function(req, res) {
-    res.send("Home Page!");
+    res.render("landing");
+});
+
+app.get('/drumsets', function(req, res) {
+    res.render("drumsets");
 });
 
 app.listen(3000, function() {
